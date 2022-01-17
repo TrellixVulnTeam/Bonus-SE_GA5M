97746db0ed7b3fa88a0fe6e6bad2e273eb288989
vntuan13579 <64194723+vntuan13579@users.noreply.github.com>
2022-01-17 19:42:44 +0700

    iter5 commit

--- fullsource/matrix-react-sdk/src/components/structures/RightPanel.tsx
+++ fullsource/matrix-react-sdk/src/components/structures/RightPanel.tsx
@@ -312,6 +312,7 @@ export default class RightPanel extends React.Component<IProps, IState> {
 
         return (
             <aside className="mx_RightPanel dark-panel" id="mx_RightPanel">
+                { card }
             </aside>
         );
     }
